680c1a8c649b5f58374b7d0ea34d15b5c4f851f9
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2025-09-04 21:06:20 +0500

    Services section done

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -1,6 +1,6 @@
+import { FaCode } from "react-icons/fa6";
 import { MotionUp } from "./ui/motion-up";
 import { HiOutlineDesktopComputer } from "react-icons/hi";
-// import { HiMiniDevicePhoneMobile } from "react-icons/hi2";
 import { LuSettings } from "react-icons/lu";
 
 export default function Services() {
@@ -21,35 +21,34 @@ export default function Services() {
 
       <div className="container servicesCardDiv lg:pb-10 pb-20 lg:mt-0 lg:px-10 px-5 mt-8 flex items-center justify-evenly min-h-[430px]">
         <MotionUp delay={0.1}>
-          <div className="serviceCard shadow transition duration-200 hover:scale-[1] hover:-translate-y-2 flex flex-col justify-center gap-5 lg:w-[520px] lg:h-[260px] h-[220px] bg-white rounded-xl border-2 border-gray-300 px-3">
+          <div className="serviceCard shadow transition duration-200 hover:scale-[1] hover:-translate-y-2 flex flex-col justify-center gap-5 lg:w-[350px] lg:h-[300px] h-[220px] bg-white rounded-lg border-2 border-gray-300 hover:border-blue-500 px-3">
+            <p className="text-gray-800 px-5 flex flex-col gap-3 lg:text-xl md:text-xl font-semibold dark:text-neutral-200">
+              <span className="flex justify-center items-center rounded-full w-[60px] p-3 bg-black text-white">
+                <FaCode className="text-4xl" />
+              </span>
+              Full Stack Development
+            </p>
+            <p className="px-5 lg:text-base md:text-base text-sm">
+               I specialize in developing full-stack web applications that combine elegant frontends with scalable backends, ensuring a smooth user experience across all devices and platforms.
+            </p>
+          </div>
+        </MotionUp>
+
+        <MotionUp delay={0.2}>
+          <div className="serviceCard shadow transition duration-200 hover:scale-[1] hover:-translate-y-2 flex flex-col justify-center gap-5 lg:w-[350px] lg:h-[300px] h-[220px] bg-white rounded-xl border-2 border-gray-300 hover:border-blue-500 px-3">
             <p className="text-gray-800 px-5 flex flex-col gap-3 lg:text-xl md:text-xl font-semibold dark:text-neutral-200">
               <span className="flex justify-center items-center rounded-full w-[60px] p-3 bg-black text-white">
                 <HiOutlineDesktopComputer className="text-4xl" />
               </span>
                Frontend Development
             </p>
-            <p className="px-5 lg:text-base md:text-base text-sm">
-               {/* I specialize in creating responsive web apps using modern tools like MERN stack and Next.js, ensuring smooth user experience. */}
-               I specialize in building responsive and dynamic web interfaces using React, Next.js, and modern UI libraries, with a strong focus on performance and seamless user experiences.</p>
+            <p className="px-5 lg:text-base md:text-base text-sm">I specialize in developing responsive and interactive websites using React, Next.js, and modern tools, with a strong focus on performance optimization and user-friendly designs.</p>
           </div>
         </MotionUp>
 
-        {/* <MotionUp delay={0.2}>
-          <div className="serviceCard shadow transition duration-200 hover:scale-[1.03] hover:-translate-y-1 flex flex-col justify-center gap-5 lg:w-[330px] lg:h-[280px] bg-white rounded-lg border border-gray-300 px-3">
-            <p className="text-gray-800 px-5 flex flex-col gap-3 lg:text-xl md:text-xl font-semibold dark:text-neutral-200">
-              <span className="flex justify-center items-center rounded-full w-[60px] p-3 bg-black text-white">
-                <HiMiniDevicePhoneMobile className="text-4xl" />
-              </span>
-              Mobile App Development
-            </p>
-            <p className="px-5">
-               I specialize in crafting highly responsive mobile apps using React Native, delivering a flawless user experience across all devices.
-            </p>
-          </div>
-        </MotionUp> */}
 
         <MotionUp delay={0.3}>
-          <div className="serviceCard shadow transition duration-200 hover:scale-[1] hover:-translate-y-2 flex flex-col justify-center gap-5 lg:w-[520px] lg:h-[260px] h-[220px] bg-white rounded-xl border-2 border-gray-300 px-3">
+          <div className="serviceCard shadow transition duration-200 hover:scale-[1] hover:-translate-y-2 flex flex-col justify-center gap-5 lg:w-[350px] lg:h-[300px] h-[220px] bg-white rounded-xl border-2 border-gray-300 hover:border-blue-500 px-3">
             <p className="text-gray-800 px-5 flex flex-col gap-3 lg:text-xl md:text-xl font-semibold dark:text-neutral-200">
               <span className="flex justify-center items-center rounded-full w-[60px] p-3 bg-black text-white">
                 <LuSettings className="text-4xl" />

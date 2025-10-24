6c18af2d1b01448afe53b17cbf2c9deef846a6b0
MuhammadHassanRaza25 <hassanejaz773@gmail.com>
2025-10-24 21:48:27 +0500

    fixed testimonial section

--- src/app/components/Testimonials.jsx
+++ src/app/components/Testimonials.jsx
@@ -5,25 +5,25 @@ export default function Testimonials() {
   const testimonialsData = [
     {
       quote:
-        "Muhammad Hassan Raza brought my vision to life with a stunning, professional portfolio website. It highlights my skills as a Graphic Designer & Video Editor and elevates my brand in an impactful way.",
+        "Muhammad Hassan Raza brought my vision to life with a stunning portfolio website. It perfectly showcases my skills as a Graphic Designer & Video Editor and elevates my brand.",
       name: "Muhammad Obaid Raza",
       title: "Graphic Designer & Video Editor",
     },
     {
       quote:
-        "Muhammad Hassan Raza's exceptional problem-solving mindset and keen attention to detail ensure every challenge is met with the most efficient and effective solutions.",
-      name: "AfaqDev",
-      title: "Full Stack Developer",
+        "Working with Muhammad Hassan Raza was a game-changer for our online presence. His expertise in frontend development resulted in cutting-edge solutions that truly stands out.",
+      name: "Intergral Technologists",
+      title: "CEO",
     },
     {
       quote:
-        "Consistently delivering high-performance full-stack solutions, Muhammad Hassan Raza combines deep technical expertise with innovative problem-solving to ensure seamless, efficient development.",
+        "Muhammad Hassan Raza consistently delivers high-performance full-stack solutions. His deep technical expertise and innovative problem-solving ensure seamless and efficient development.",
       name: "Muhammad Ahmed",
       title: "Full Stack Developer",
     },
     {
       quote:
-        "Muhammad Hassan Raza is a true expert in building top-tier Full Stack solutions, delivering innovative and highly efficient results. His unmatched speed and precision ensure exceptional outcomes every time.",
+        "A true expert in Full Stack development, Muhammad Hassan Raza delivers innovative, efficient results with unmatched speed and precision, ensuring exceptional outcomes every time.",
       name: "Bilal Ahmed",
       title: "Software Engineer",
     },
@@ -44,7 +44,7 @@ export default function Testimonials() {
         </MotionUp>
       </div>
 
-      <div className="container flex items-center justify-center lg:mt-0 lg:px-0 px-5 mt-8 min-h-[340px]">
+      <div className="container flex items-center justify-center pb-10 lg:mt-0 lg:px-0 px-5 mt-8 min-h-[380px]">
         <MotionUp delay={0.1}>
           <InfiniteMovingCards2
             items={testimonialsData}
